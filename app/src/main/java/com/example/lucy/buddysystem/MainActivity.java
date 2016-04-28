daf93da78a1173c9efb0f6fdc3a4f46923f8a7cb
Lucy Wyman <wyman.lucy@gmail.com>
2016-04-28 11:31:08 -0700

    Add items to front page

--- app/src/main/java/com/example/lucy/buddysystem/MainActivity.java
+++ app/src/main/java/com/example/lucy/buddysystem/MainActivity.java
@@ -4,8 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.content.Intent;
 import android.support.v7.widget.Toolbar;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +28,18 @@ public class MainActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
         toolbar.inflateMenu(R.menu.menu_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.messages);
+        fab.setOnClickListener(new View.OnClickListener() {
+            // TODO
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
