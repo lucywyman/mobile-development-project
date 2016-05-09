d731053e8c672d7e6e1289a97a0cd51309c50279
Lucy Wyman <wyman.lucy@gmail.com>
2016-05-09 13:00:10 -0700

    Start to add messaging layout

--- app/src/main/java/com/example/lucy/buddysystem/SettingsActivity.java
+++ app/src/main/java/com/example/lucy/buddysystem/SettingsActivity.java
@@ -2,7 +2,8 @@ package com.example.lucy.buddysystem;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import java.util.List;
+import android.widget.Toolbar;
+
 
 /**
  * Created by lucy on 4/25/16.
@@ -12,21 +13,11 @@ public class SettingsActivity extends PreferenceActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            //setSupportActionBar(toolbar);
+            //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
-
-/*    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.xml.settings);
-    }
-    @Override
-    public void onBuildHeaders(List<Header> target)
-    {
-        loadHeadersFromResource(R.xml.settings_headers, target);
-    }
-    @Override
-    protected boolean isValidFragment(String fragmentName) {
-        return SettingsFragment.class.getName().equals(fragmentName);
-    }*/
 }
 
