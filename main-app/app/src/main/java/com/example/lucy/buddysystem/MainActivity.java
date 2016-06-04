5930376e3f7165082b62591d370d1522248a74f2
Lucy Wyman <wyman.lucy@gmail.com>
2016-06-04 13:16:42 -0700

    Initial sundown notification commit

--- main-app/app/src/main/java/com/example/lucy/buddysystem/MainActivity.java
+++ main-app/app/src/main/java/com/example/lucy/buddysystem/MainActivity.java
@@ -1,24 +1,42 @@
 package com.example.lucy.buddysystem;
 
+import java.text.ParseException;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.content.Intent;
 import android.support.v7.widget.Toolbar;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private Date sundown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DateFormat df = new SimpleDateFormat("h:mm:ss a");
+        Date sunsetdate = getSundown(this);
+        String sunset = "Sunset today is at " + df.format(sunsetdate);
+        TextView myTextView= (TextView) findViewById(R.id.myTextView);
+        myTextView.setText(sunset);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         if(toolbar != null) {
@@ -27,17 +45,48 @@ public class MainActivity extends AppCompatActivity {
             getSupportActionBar().setHomeButtonEnabled(true);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
-        toolbar.inflateMenu(R.menu.menu_main);
+        if (toolbar != null) {
+            toolbar.inflateMenu(R.menu.menu_main);
+        }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.messages);
-        fab.setOnClickListener(new View.OnClickListener() {
-            // TODO
+        sundown = getSundown(MainActivity.this);
+        new SundownNotifier(this).execute(sundown);
+    }
+
+    private Date getSundown(Context context) {
+        RequestQueue queue = Volley.newRequestQueue(context);
+        String url ="http://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        JSONObject json = null;
+                        try {
+                            json = new JSONObject(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        String sunset = null;
+                        try {
+                            sunset = json.getString("sunset");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        DateFormat df = new SimpleDateFormat("h:mm:ss a");
+                        try {
+                            sundown = df.parse(sunset);
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onErrorResponse(VolleyError error) {
             }
         });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+        return sundown;
     }
 
 
---
+++ main-app/app/src/main/java/com/example/lucy/buddysystem/SundownNotifier.java
@@ -0,0 +1,44 @@
+package com.example.lucy.buddysystem;
+
+import android.content.Intent;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v4.app.NotificationCompat;
+import android.app.NotificationManager;
+import java.util.Date;
+
+public class SundownNotifier extends AsyncTask<Date, Void, Void> {
+    private Context ctx;
+    PendingIntent contentIntent;
+
+    public SundownNotifier(Context ctx){
+        this.ctx = ctx;
+    }
+
+    protected void onPreExecute() {
+        Intent notificationIntent = new Intent(ctx, MainActivity.class);
+        contentIntent = PendingIntent.getActivity(ctx, 0, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    @Override
+    protected Void doInBackground(Date... sundown) {
+        this.ctx = ctx.getApplicationContext();
+        Date now = new Date();
+        //if(now.equals(sundown)){
+            NotificationCompat.Builder builder =
+                    new NotificationCompat.Builder(ctx)
+                            .setSmallIcon(R.drawable.ic_launcher)
+                            .setContentTitle("It's getting dark")
+                            .setContentText("Find a buddy!")
+                            .setAutoCancel(true);
+
+            builder.setContentIntent(contentIntent);
+            // Add as notification
+            NotificationManager manager = (NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+            manager.notify(8001, builder.build());
+        //}
+        return null;
+    }
+}
